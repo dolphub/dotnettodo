2a15e7f90aa81fc41d9d2bf47426fded05d4ac83
admin <harry@desully.com>
2017-10-25 23:05:27 -0400

    Added required key

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -11,14 +11,14 @@ namespace todoapi.Models {
             CreatedAt = now;
         }
 
-        [Key]
+        [Key, Required]
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
         public DateTime LastModifiedAt { get; set; }
         public DateTime CreatedAt { get; set; }
 
-        public List<Tag> Tags { get; set; }
+        public virtual List<Tag> Tags { get; set; }
 
         public TodoItem Copy() {
             return new TodoItem { Id = this.Id, Name = this.Name, IsComplete = this.IsComplete };
