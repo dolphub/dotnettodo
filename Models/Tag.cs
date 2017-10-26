8dde72b79e1d5de68dea973611474894c0bfe1a9
admin <harry@desully.com>
2017-10-25 22:58:05 -0400

    Tag

---
+++ Models/Tag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoapi.Models
+{
+    public class Tag
+    {
+
+        public Tag()
+        {
+
+        }
+
+        [Key]
+        public int TagID { get; set; }
+
+        public string TagName { get; set; }
+
+        public int? TodoID { get; set; }
+
+        [ForeignKey("TodoID")]
+        public virtual TodoItem ToDo { get; set; }
+    }
+}
--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace todoapi.Models {
     public class TodoItem {
@@ -8,11 +10,15 @@ namespace todoapi.Models {
             LastModifiedAt = now;
             CreatedAt = now;
         }
+
+        [Key]
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
         public DateTime LastModifiedAt { get; set; }
         public DateTime CreatedAt { get; set; }
+
+        public List<Tag> Tags { get; set; }
 
         public TodoItem Copy() {
             return new TodoItem { Id = this.Id, Name = this.Name, IsComplete = this.IsComplete };
